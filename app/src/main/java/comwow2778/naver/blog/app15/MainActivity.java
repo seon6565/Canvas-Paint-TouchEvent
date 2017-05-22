5770a46588912c394f46a45ac06f72304c83b016
seon6565 <wow2778@naver.com>
2017-05-22 23:06:41 +0900

    완성본ver.1

--- app/src/main/java/comwow2778/naver/blog/app15/MainActivity.java
+++ app/src/main/java/comwow2778/naver/blog/app15/MainActivity.java
@@ -1,45 +1,136 @@
 package comwow2778.naver.blog.app15;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.GestureDetector;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 public class MainActivity extends AppCompatActivity {
     CheckBox checkBox;
     MyPainter mypainter;
+    int check = 0;
+    int check_blur = 0;
+    int check_color = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mypainter = (MyPainter)findViewById(R.id.canvas);
         checkBox = (CheckBox)findViewById(R.id.checkBox);
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                checkBox.setChecked(isChecked);
+                mypainter.IsChecked = isChecked;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0, 1, 0, "Bluring").setCheckable(true);
+        menu.add(0, 2, 0, "Coloring").setCheckable(true);
+        menu.add(0, 3, 0, "Pen Width Big").setCheckable(true);
+        menu.add(0, 4, 0, "Pen Color Red");
+        menu.add(0, 5, 0, "Pen Color Blue");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == 1) {
+            check_blur++;
+            if (check_blur % 2 == 1) {
+                mypainter.setOperation("bluring");
+                item.setChecked(true);
+            } else {
+                mypainter.setOperation("nofilter");
+                item.setChecked(false);
+            }
+
+        }
+        else if (item.getItemId() == 2) {
+            check_color++;
+            if (check_color % 2 == 1) {
+                item.setChecked(true);
+                mypainter.setOperation("coloring");
+
+            } else {
+                mypainter.setOperation("nofilter");
+                item.setChecked(false);
+            }
+
+        }
+        else if (item.getItemId() == 3) {
+            check++;
+            if (check % 2 == 1) {
+                item.setChecked(true);
+                mypainter.setOperation("big");
+            } else {
+                item.setChecked(false);
+                mypainter.setOperation("small");
+            }
+        }
+        else if (item.getItemId() == 4) {
+            mypainter.setOperation("red");
+        }
+        else if (item.getItemId() == 5) {
+            mypainter.setOperation("blue");
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public String getExternalPath() {
+        String sdPath = "";
+        String ext = Environment.getExternalStorageState();
+        if (ext.equals(Environment.MEDIA_MOUNTED)) {
+            sdPath = Environment.getExternalStorageDirectory().getAbsolutePath() +
+                    "/";
+            //sdPath = "/mnt/sdcard/";
+        } else
+            sdPath = getFilesDir() + "";
+        //Toast.makeText(this, sdPath, Toast.LENGTH_SHORT).show();
+        return sdPath;
     }
 
 
     public void onClick(View v){
         if(v.getId()==R.id.b1) {
-            mypainter.clear();
+            mypainter.setOperation("clear");
         }
         else if(v.getId()==R.id.b2){
-
+            mypainter.setOperation("open");
         }
         else if(v.getId()==R.id.b3){
-
+            mypainter.setOperation("save");
+            mypainter.path = getFilesDir() + "";
         }
         else if(v.getId()==R.id.b4){
-
+            checkBox.setChecked(true);
+            mypainter.IsChecked = true;
+            mypainter.setOperation("rotate");
         }
         else if(v.getId()==R.id.b5){
-
+            mypainter.IsChecked = true;
+            checkBox.setChecked(true);
+            mypainter.setOperation("move");
         }
         else if(v.getId()==R.id.b6){
-
+            mypainter.IsChecked = true;
+            checkBox.setChecked(true);
+            mypainter.setOperation("scale");
         }
         else if(v.getId()==R.id.b7){
-
+            mypainter.IsChecked = true;
+            checkBox.setChecked(true);
+            mypainter.setOperation("skew");
         }
     }
 }
--- app/src/main/java/comwow2778/naver/blog/app15/MyPainter.java
+++ app/src/main/java/comwow2778/naver/blog/app15/MyPainter.java
@@ -3,14 +3,19 @@ package comwow2778.naver.blog.app15;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -23,11 +28,19 @@ import java.io.IOException;
 public class MyPainter extends View {
     Bitmap mBitmap;
     Canvas mCanvas;
+    String operation = "";
+    String command = "";
+    String path = "";
+    String option = "";
+    int width = 0, height = 0;
+    Boolean IsSelected = false;
+    int cx= -1, cy = -1;
     Paint mPaint = new Paint();
+    boolean IsChecked = false;
+    int sx=-1, sy=-1;
 
     public MyPainter(Context context) {
         super(context);
-        mPaint.setColor(Color.BLACK);
     }
 
     public MyPainter(Context context, @Nullable AttributeSet attrs) {
@@ -40,69 +53,160 @@ public class MyPainter extends View {
         super.onSizeChanged(w, h, oldw, oldh);
         mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
         mCanvas = new Canvas();
+        mPaint.setStrokeWidth(3);
         mCanvas.setBitmap(mBitmap);
         mCanvas.drawColor(Color.YELLOW);
     }
-
-    private void stamp(){
+    private void drawStamp(){
+        Matrix matrix = new Matrix();
         Bitmap img = BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher);
-        mCanvas.drawBitmap(img,oldx,oldy,mPaint);
+        width = img.getWidth();
+        height = img.getHeight();
+        mCanvas.save();
+        if(operation.equals("rotate")){
+            mCanvas.rotate(30,mCanvas.getWidth()/2,mCanvas.getHeight()/2);
+        }
+        else if(operation.equals("move"))
+            mCanvas.translate(10,10);
+        else if(operation.equals("scale")){
+            mCanvas.scale(1.5f,1.5f);
+            sx= (int)(cx / 1.5 - (width /2)) ;sy=(int)(cy / 1.5- (height /2));
+        }
+        else if(operation.equals("skew"))
+            mCanvas.skew(0.2f,0.0f);
+        mCanvas.drawBitmap(img,sx,sy,mPaint);
+    }
+
+    public void setOperation(String op){
+        if(op.equals("save")) this.command = op;
+        else if(op.equals("clear")) this.command = op;
+        else if(op.equals("open")) this.command = op;
+        else if(op.equals("bluring")) this.option = op;
+        else if(op.equals("coloring")) this.option = op;
+        else if(op.equals("nofilter")) this.option = op;
+        else if(op.equals("big")) this.option =op;
+        else if(op.equals("small")) this.option =op;
+        else if(op.equals("blue")) this.option = op;
+        else if(op.equals("red")) this.option = op;
+        else {this.operation = op; IsSelected = true;}
+        invalidate();
+    }
+    public boolean Save(String file_name){
+        try{
+            FileOutputStream out = new FileOutputStream(file_name);
+            mBitmap.compress(Bitmap.CompressFormat.JPEG,100,out);
+            out.close();
+            return true;
+        } catch (FileNotFoundException e) {
+            Log.e("FileNotFoundException",e.getMessage());
+        } catch (IOException e) {
+            Log.e("IOException",e.getMessage());
+        }
+        return false;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        if(mBitmap != null) {
-            canvas.drawBitmap(mBitmap, 10, 10, null);
+
+        if(IsChecked) {
+            if(!IsSelected){
+                drawStamp();
+                operation="";
+                mCanvas.restore();}
+            else{
+                IsSelected = false;
+
+            }
         }
+
+        if(command.equals("clear")) {
+            mBitmap.eraseColor(Color.parseColor("#fdf39a"));
+        }
+        if(command.equals("save")){
+
+            Save(path + "img.jpg");
+            Toast.makeText(getContext(),"SAVE",Toast.LENGTH_SHORT).show();
+        }
+
+        if(command.equals("open")){
+            Toast.makeText(getContext(),"OPEN",Toast.LENGTH_SHORT).show();
+            Bitmap storedImg = BitmapFactory.decodeFile("/data/data/comwow2778.naver.blog.app15/img.jpg");
+
+            if(storedImg !=null){
+                mBitmap.eraseColor(Color.parseColor("#fdf39a"));
+                int width = storedImg.getWidth();
+                int height = storedImg.getHeight();
+
+                Bitmap sBitmap = Bitmap.createScaledBitmap(storedImg,width/2,
+                        height/2,false);
+                int x1 = mCanvas.getWidth() /2 - sBitmap.getWidth()/2;
+                int y1 = mCanvas.getHeight()/2 - sBitmap.getHeight()/2;
+                mCanvas.drawBitmap(sBitmap,x1,y1,mPaint);
+            }
+            else Toast.makeText(getContext(),"저장된 파일이 없습니다",Toast.LENGTH_SHORT).show();
+        }
+
+        if(option.equals("bluring")){
+            BlurMaskFilter blur = new BlurMaskFilter(50,BlurMaskFilter.Blur.OUTER);
+            mPaint.setMaskFilter(blur);
+        }
+        if(option.equals("coloring")){
+            float[] matrix_array = {
+                    2f, 0f, 0f, 0f, -25f,
+                    0f, 2f, 0f, 0f, -25f,
+                    0f, 0f, 2f, 0f, -25f,
+                    0f, 0f, 0f, 1f, 0f
+            };
+            ColorMatrix matrix = new ColorMatrix(matrix_array);
+            mPaint.setColorFilter(new ColorMatrixColorFilter(matrix));
+        }
+        if(option.equals("nofilter")){
+            mPaint.reset();
+        }
+        if(option.equals("big")) {mPaint.setStrokeWidth(5);}
+        if(option.equals("small")) mPaint.setStrokeWidth(3);
+        if(option.equals("red")) mPaint.setColor(Color.RED);
+        if(option.equals("blue")) mPaint.setColor(Color.BLUE);
+        command="";
+        option="";
+        canvas.drawBitmap(mBitmap,0,0,null);
     }
+
     int oldx = -1; int oldy = -1;
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        int x = (int)event.getX();
-        int y = (int)event.getY();
+        if(!IsChecked){
+            int x = (int)event.getX();
+            int y = (int)event.getY();
 
-        if(event.getAction() == MotionEvent.ACTION_DOWN){
-            oldx =x; oldy=y;
-        }
-        else if(event.getAction() == MotionEvent.ACTION_MOVE){
-            if(oldx != -1) {
-                mCanvas.drawLine(oldx, oldy, x, y, mPaint);
-                invalidate();
-                oldx = x;
-                oldy = y;
+            if(event.getAction() == MotionEvent.ACTION_DOWN){
+                oldx =x; oldy = y;
             }
-
+            else if(event.getAction() == MotionEvent.ACTION_MOVE){
+                if (oldx != -1){
+                    mCanvas.drawLine(oldx,oldy,x,y,mPaint);
+                    invalidate();
+                    oldx = x; oldy = y;
+                }
+            }
+            else if(event.getAction() == MotionEvent.ACTION_UP){
+                if (oldx != -1){
+                    mCanvas.drawLine(oldx,oldy,x,y,mPaint);
+                    invalidate();
+                }
+                oldx = -1; oldy = -1;
+            }
         }
-        else if(event.getAction() == MotionEvent.ACTION_UP){
-            if(oldx != -1) {
-                mCanvas.drawLine(oldx, oldy, x, y, mPaint);
+        else{
+            cx = (int)event.getX();
+            cy = (int)event.getY();
+            if(event.getAction() == MotionEvent.ACTION_DOWN){
+                sx= cx - (width/2) ;sy=cy - (height/2);
                 invalidate();
             }
-            oldx = -1;
-            oldy = -1;
         }
-
         return true;
     }
 
-
-    public void clear() {
-        mBitmap.eraseColor(Color.WHITE);
-        invalidate();
-    }
-
-    public boolean Save(String file_name) {
-        try {
-            FileOutputStream out = new FileOutputStream(file_name);
-            mBitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
-            out.close();
-            return true;
-        } catch (FileNotFoundException e) {
-            Log.e("FileNotFoundException", e.getMessage());
-        } catch (IOException e) {
-            Log.e("IOException", e.getMessage());
-        }
-        return false;
-    }
 }
